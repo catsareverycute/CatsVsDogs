466dea71b328b7ff71b51969afba8e1010774de7
BT_2S14_29 <corinaq3@nycstudents.net>
2024-11-12 10:48:04 -0500

    started introduction to simulator, adding methods based on what's needed right now

--- src/Campaign.java
+++ src/Campaign.java
@@ -1,13 +1,53 @@
+import java.util.HashMap;
+
 public class Campaign {
 private int day = 1;
-private int vote = 1;
+private int yourVotes = 0;
+private int oppVotes = 0;
+private double budget = 0;
 private String name;
 private String party;
+private HashMap<String, Integer> catStates = new HashMap<String, Integer>();
+private HashMap<String, Integer> dogStates = new HashMap<String, Integer>();
+private HashMap<String, Integer> swingStates = new HashMap<String, Integer>();
+
     public Campaign(String name, String party) {
         this.name = name;
         this.party = party;
+        catStates.put("Vermont", 0);
+        catStates.put("Maine", 0);
+        catStates.put("Massachusetts", 0);
+        catStates.put("Rhode Island", 0);
+        catStates.put("New Hampshire", 0);
+        dogStates.put("Idaho", 0);
+        dogStates.put("Montana", 0);
+        dogStates.put("Arkansas", 0);
+        dogStates.put("Mississippi", 0);
+        dogStates.put("Oklahoma", 0);
+        swingStates.put("New York", 0);
+        swingStates.put("Delaware", 0);
+        swingStates.put("Pennsylvania", 0);
+        swingStates.put("Idaho", 0);
+        swingStates.put("Wyoming",0);
+        if (party.equals("cat")){
+            for (String i : catStates.keySet()) {
+                catStates.replace(i, 10000000);
+            }
+        }
+        else {
+            for (String i : dogStates.keySet()) {
+                dogStates.replace(i, 10000000);
+            }
+        }
+
     }
+
     public int showDay(){
         return day;
     }
+
+    public double showBudget(){
+        return budget;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.HashMap;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
+        DecimalFormat format;
+        format = new DecimalFormat("#.##");
         Scanner s = new Scanner(System.in);
         System.out.println("Welcome. You will be a candidate for the election of 4202. Please input your information before you can start.");
         System.out.println("What is your name?");
@@ -13,19 +17,26 @@ public class Main {
             party = s.nextLine().toLowerCase();
         }
         if (party.equals("cat")) {
-            System.out.println("Welcome to the Cat Party! We are cuter than dogs and love being indoors.\n" +
-                    " ╱|、\n" +
-                    "(˚ˎ 。7  \n" +
-                    " |、˜〵          \n" +
-                    " じしˍ,)ノ");
+            System.out.println("""
+                    Welcome to the Cat Party! We are cuter than dogs and love being indoors.
+                     ╱|、
+                    (˚ˎ 。7 \s
+                     |、˜〵         \s
+                     じしˍ,)ノ🐟""");
         }
         else {
-            System.out.println("Welcome to the Dog Party! We are cuter than cats and love being outdoors.\n" +
-                    "૮˶• ﻌ •˶ა\n" +
-                    "./づ~ \uD83E\uDDB4");
+            System.out.println("""
+                    Welcome to the Dog Party! We are cuter than cats and love being outdoors.
+                    ૮˶• ﻌ •˶ა
+                    ./づ~ 🦴""");
         }
         // insert here
+        HashMap<String, Integer> stateVotes = new HashMap<String, Integer>();
+        stateVotes.put("Vermont",0);
         Campaign hi = new Campaign(name, party);
-        System.out.println("Day " + hi.showDay() + ": " + (16 - hi.showDay()) + " days before the debate, " + (31 - hi.showDay()) + " days before Election Day.");
+        System.out.println("\nDay " + hi.showDay() + ": " + (21 - hi.showDay()) + " days before the debate, " + (31 - hi.showDay()) + " days before Election Day.");
+        System.out.println("Your budget is: " + format.format(hi.showBudget())+ ", what do you want to do today?");
+        System.out.println("\n1. Post Promotion (Gain money for other campaigning options)\n2. Hire Workers (For $1,000, hire a social media manager to post for you)\n3. Travel to State (For $10,000, campaign at a chosen state)\n4. View Posts (Shows posts YOU made and their statistics)\n5. Sleep");
+
     }
 }
